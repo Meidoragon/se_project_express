fa590aedfdc280eba305bff5f5ab2b8c4057bd7c
Meidoragon <104027002+Meidoragon@users.noreply.github.com>
2024-01-03 16:33:17 -0600

    Add: Get items and update item endpoints

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -13,6 +13,25 @@ const createItem = (req, res) => {
   })
 }
 
+const getItems = (req, res) => {
+  ClothingItem.find({}).then((items) => res.status(200).send(items))
+  .catch((err) => {
+      res.status(500).send({message: `Error from getItems: ${err}`});
+    })
+}
+
+const updateItem = (req, res) => {
+  const {itemId} = req.params;
+  const {imageUrl} = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, {$set: {imageUrl}}).orFail().then((item) => res.status(200).send({data: item}))
+  .catch((err) => {
+    res.status(500).send({message: `Error from updateItem: ${err}`});
+  })
+}
+
 module.exports = {
-  createItem
+  createItem,
+  getItems,
+  updateItem
 }
--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -1,10 +1,18 @@
 const router  = require('express').Router();
 
-const { createItem } = require('../controllers/clothingItems')
+const { createItem, getItems, updateItem } = require('../controllers/clothingItems')
 
 //CRUD
 
 //Create
 router.post('/', createItem)
+
+//Read
+router.get('/', getItems)
+
+//Update
+router.put('/:itemId', updateItem)
+//Delete
+
 
 module.exports = router;
